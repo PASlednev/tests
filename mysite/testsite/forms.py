fe8b40bda6308878e1c84e92c09f3df52799f036
PASlednev <DMB462@yandex.ru>
2022-07-27 00:47:13 +0300

    make model

--- mysite/testsite/forms.py
+++ mysite/testsite/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 from .models import *
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
 class UserRegisterForm(UserCreationForm):
--- mysite/testsite/views.py
+++ mysite/testsite/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
+from django.contrib.auth import login, logout
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 
-from .forms import QuestionsForm, UserRegisterForm
+from .forms import QuestionsForm, UserRegisterForm, UserLoginForm
 from .models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -51,8 +52,21 @@ def contact(request):
     return HttpResponse("Обратная связь")
 
 
-def login(request):
-    return render(request, 'testsite/login.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm
+    return render(request, 'testsite/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 
 def register(request):
